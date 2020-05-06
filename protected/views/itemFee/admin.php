fc2688be1ce9acc8d996bff9de94c39a82d6fc6b
Walter Céspedes A <analisis2.sistemas@simoniz.com.co>
2020-05-06 13:05:52 -0500

    Commit upload crud Items FEE pt2
    Commit upload crud Items FEE pt2

---
+++ protected/views/itemFee/admin.php
@@ -0,0 +1,95 @@
+<?php
+/* @var $this ItemFeeController */
+/* @var $model ItemFee */
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle('fast');
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#item-fee-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+
+//para combos de usuarios
+$lista_usuarios = CHtml::listData($usuarios, 'Usuario', 'Usuario'); 
+
+?>
+
+<h3>Administración de items (FEE)</h3>
+
+<div class="btn-group" style="padding-bottom: 2%">
+   <button type="button" class="btn btn-success" onclick="location.href = '<?php echo Yii::app()->getBaseUrl(true).'/index.php?r=itemFee/create'; ?>';"><i class="fa fa-plus"></i> Nuevo registro</button>
+    <button type="button" class="btn btn-success search-button"><i class="fa fa-filter"></i> Busqueda avanzada</button>
+</div>
+
+<div class="search-form" style="display:none;">
+<?php $this->renderPartial('_search',array(
+	'model'=>$model,
+    'lista_usuarios' => $lista_usuarios,
+)); ?>
+</div><!-- search-form -->
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'item-fee-grid',
+	'dataProvider'=>$model->search(),
+	//'filter'=>$model,
+    'enableSorting' => false,
+	'columns'=>array(
+		'Id_Fee_Item',
+		'Rowid_Item',
+		array(
+            'name'=>'Rowid_Item',
+            'value'=>'UtilidadesVarias::DescItem($data->Rowid_Item)',
+        ),
+		array(
+            'name'=>'Porcentaje',
+            'value'=>function($data){
+                return number_format($data->Porcentaje, 2);
+            },
+            'htmlOptions'=>array('style' => 'text-align: right;'),
+        ),
+		/*array(
+            'name'=>'Id_Usuario_Creacion',
+            'value'=>'$data->idusuariocre->Usuario',
+        ),
+        array(
+            'name'=>'Fecha_Creacion',
+            'value'=>'UtilidadesVarias::textofechahora($data->Fecha_Creacion)',
+        ),
+        array(
+            'name'=>'Id_Usuario_Actualizacion',
+            'value'=>'$data->idusuarioact->Usuario',
+        ),
+        array(
+            'name'=>'Fecha_Actualizacion',
+            'value'=>'UtilidadesVarias::textofechahora($data->Fecha_Actualizacion)',
+        ),*/
+        array(
+            'name' => 'Estado',
+            'type' => 'raw',
+            'value' => '($data->Estado == "1") ? "Activo" : "Inactivo"',
+        ),
+		array(
+			'class'=>'CButtonColumn',
+            'template'=>'{view}{update}',
+            'buttons'=>array(
+                'view'=>array(
+                    'label'=>'<i class="fa fa-eye actions text-black"></i>',
+                    'imageUrl'=>false,
+                    'options'=>array('title'=>'Visualizar'),
+                ),
+                'update'=>array(
+                    'label'=>'<i class="fa fa-pencil actions text-black"></i>',
+                    'imageUrl'=>false,
+                    'options'=>array('title'=>'Actualizar'),
+                    'visible'=> '(Yii::app()->user->getState("permiso_act") == true)',
+                ),
+            )
+		),
+	),
+)); ?>
